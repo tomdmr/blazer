8fc11dd011dfa4bede8ca68482a3914d93c455ec
tom (tux) <thd@mnet-online.de>
2021-05-01 20:54:40 +0200

    Extended parser

--- mockup/greeter.pl
+++ mockup/greeter.pl
@@ -7,6 +7,12 @@
 #
 # USAGE:
 # websocketd --devconsole --port 8080 --loglevel=fatal ./greeter.pl
+# Emulator for a real ESP32.
+# It understands the verbs
+#   setX, sets an LED, where X is the channel 0-2
+#   clrX, clears an LED
+#   SETaaa, where abc set (a=1) or clear (a=0) an LED
+#   CLR,  unset al LEDs
 #
 use strict;
 
@@ -14,24 +20,26 @@ use strict;
 use IO::Handle;
 STDOUT->autoflush(1);
 my $state = "000";
+print STDERR "Startup\n";
 
-# For each line FOO received on STDIN, respond with "Hello FOO!".
 while (<>) {
+    print STDERR "Got request\n";
   chomp; # remove \n
   my $cmd = $_;
   if ($cmd =~ /set([0-2])/){
 	  my $c = $1;
       substr($state, $c, 1) ="1";
   }
-  else if ($cmd =~ /clr([0-2])/){
+  if ($cmd =~ /clr([0-2])/){
 	  my $c = $1;
       substr($state, $c, 1) ="0";
   }
-  else if($cmd =~ /SET([0-1])([0-1])([0-1])/){
-      $state = $1 ~ $2 ~ $3;
+  if($cmd =~ /SET([0-1])([0-1])([0-1])/){
+      $state = $1 . $2 . $3;
   }
-  else if($cmd =~ /CLR/){
+  if($cmd =~ /CLR/){
       $state = '000';
   }
   print "STATE$state\n";
+  print STDERR "STATE$state\n";
 }
