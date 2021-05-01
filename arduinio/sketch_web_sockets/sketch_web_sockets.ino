9191b6430ae66eeafaf715c47bda1f76e7456d7a
tom (tux) <thd@mnet-online.de>
2021-05-01 10:23:08 +0200

    Moved

---
+++ arduinio/sketch_web_sockets/sketch_web_sockets.ino
@@ -0,0 +1,317 @@
+/*********
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp32-websocket-server-arduino/
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*********/
+
+// Import required libraries
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+
+// Replace with your network credentials
+const char* ssid = "WLAN 7360";
+const char* password = "41209353118149546763";
+
+//bool ledState = 0; 
+bool ledState[3] = {0, 0, 0};
+const int ledPins[3] = {19, 18, 17};
+// Touch is on GPIO15, TOUCH3
+bool touchDetected = false;
+// Timer section
+volatile int interruptCounter;
+int totalInterruptCounter;
+hw_timer_t *timer = NULL;
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <title>Blazer Web Server</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="data:,">
+  <style>
+  html {
+    font-family: Arial, Helvetica, sans-serif;
+    text-align: center;
+  }
+  h1 {
+    font-size: 1.8rem;
+    color: white;
+  }
+  h2{
+    font-size: 1.5rem;
+    font-weight: bold;
+    color: #143642;
+  }
+  .topnav {
+    overflow: hidden;
+    background-color: #143642;
+  }
+  body {
+    margin: 0;
+  }
+  .content {
+    padding: 30px;
+    max-width: 600px;
+    margin: 0 auto;
+  }
+  .card {
+    background-color: #F8F7F9;;
+    box-shadow: 2px 2px 12px 1px rgba(140,140,140,.5);
+    padding-top:10px;
+    padding-bottom:20px;
+  }
+  .button {
+    padding: 15px 50px;
+    font-size: 24px;
+    text-align: center;
+    outline: none;
+    color: #fff;
+    background-color: #0f8b8d;
+    border: none;
+    border-radius: 5px;
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    -webkit-tap-highlight-color: rgba(0,0,0,0);
+   }
+   /*.button:hover {background-color: #0f8b8d}*/
+   .button:active {
+     background-color: #0f8b8d;
+     box-shadow: 2 2px #CDCDCD;
+     transform: translateY(2px);
+   }
+   .state {
+     font-size: 1.5rem;
+     color:#8c8c8c;
+     font-weight: bold;
+   }
+  </style>
+<title>ESP Web Server</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" href="data:,">
+</head>
+<body>
+  <div class="topnav">
+    <h1>ESP WebSocket Server</h1>
+  </div>
+  <div class="content">
+    <div class="card">
+      <h2>Output - GPIO 2</h2>
+      <p class="state">state: <span id="state">%STATE%</span></p>
+      <p><button id="button" class="button">Toggle</button></p>
+    </div>
+  </div>
+<script>
+  var gateway = `ws://${window.location.hostname}/ws`;
+  var websocket;
+  window.addEventListener('load', onLoad);
+  function initWebSocket() {
+    console.log('Trying to open a WebSocket connection...');
+    websocket = new WebSocket(gateway);
+    websocket.onopen    = onOpen;
+    websocket.onclose   = onClose;
+    websocket.onmessage = onMessage; // <-- add this line
+  }
+  function onOpen(event) {
+    console.log('Connection opened');
+  }
+  function onClose(event) {
+    console.log('Connection closed');
+    setTimeout(initWebSocket, 2000);
+  }
+  function onMessage(event) {
+    var state;
+    if (event.data == "1"){
+      state = "ON";
+    }
+    else{
+      state = "OFF";
+    }
+    document.getElementById('state').innerHTML = state;
+  }
+  function onLoad(event) {
+    initWebSocket();
+    initButton();
+  }
+  function initButton() {
+    document.getElementById('button').addEventListener('click', toggle);
+  }
+  function toggle(){
+    websocket.send('toggle');
+  }
+</script>
+</body>
+</html>
+)rawliteral";
+/*
+ * Timer Service routine
+ */
+void IRAM_ATTR onTimer() {
+  portENTER_CRITICAL_ISR(&timerMux);
+  interruptCounter++;
+  portEXIT_CRITICAL_ISR(&timerMux);
+ 
+}
+ 
+
+void notifyClients() {
+  char sString[12];
+  sString[0]='S';
+  sString[1]='T';
+  sString[2]='A';
+  sString[3]='T';
+  sString[4]='E';
+
+  sString[5]='0' | ledState[0];
+  sString[6]='0' | ledState[1];
+  sString[7]='0' | ledState[2];
+  sString[8]= 0;
+
+  //sprintf(sString, "STATE%d%d%d", ledState[0], ledState[1], ledState[2]);
+  ws.textAll(sString);
+}
+
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
+  AwsFrameInfo *info = (AwsFrameInfo*)arg;
+  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+    data[len] = 0;
+    Serial.printf("Got cmd %s\n", data);
+    int len = strlen((char*)data);
+    if (strcmp((char*)data, "STATE") == 0) {
+      notifyClients();
+    }
+    if((len>3)&&(strncmp((char*)data, "set", 3)==0)){
+      int s = data[3]-'0';
+      if(s>=0 && s<3){
+        ledState[s] = 1;
+      }
+      notifyClients();
+    }
+    if((len>3)&&(strncmp((char*)data, "clr", 3)==0)){
+      int s = data[3]-'0';
+      if(s>=0 && s<3){
+        ledState[s] = 0;
+      }
+      notifyClients();
+    }
+    if((len>6)&&(strncmp((char*)data, "SET",3)==0)){
+      for(int i=0; i<3; i++){
+        int s = data[3+i];
+        ledState[i] = s ? 1 : 0;
+      }
+      notifyClients();
+    }
+    if(strcmp((char*)data, "CLR")==0){
+      ledState[0] = ledState[1] = ledState[2]=0;
+      notifyClients();
+    }
+  }
+}
+
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
+             void *arg, uint8_t *data, size_t len) {
+  switch (type) {
+    case WS_EVT_CONNECT:
+      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+      break;
+    case WS_EVT_DISCONNECT:
+      Serial.printf("WebSocket client #%u disconnected\n", client->id());
+      break;
+    case WS_EVT_DATA:
+      handleWebSocketMessage(arg, data, len);
+      break;
+    case WS_EVT_PONG:
+      Serial.printf("PONG\n");
+    case WS_EVT_ERROR:
+      break;
+  }
+}
+
+void initWebSocket() {
+  ws.onEvent(onEvent);
+  server.addHandler(&ws);
+}
+
+String processor(const String& var){
+  Serial.println(var);
+  if(var == "STATE"){
+    if (ledState){
+      return "ON";
+    }
+    else{
+      return "OFF";
+    }
+  }
+}
+
+void setup(){
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+  for(int i=0; i<3; i++){
+    pinMode(     ledPins[i], OUTPUT);
+    digitalWrite(ledPins[i], LOW);    
+  }
+  
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(900);
+    digitalWrite(ledPins[0], HIGH);
+    delay(100);
+    digitalWrite(ledPins[0], LOW);
+    Serial.println("Connecting to WiFi..");
+  }
+  // Print ESP Local IP Address
+  Serial.println(WiFi.localIP());  
+  // Timer
+  timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(timer, &onTimer, true);
+  timerAlarmWrite(timer, 60000000, true);
+  timerAlarmEnable(timer);
+  // Web-Server/Sockets
+  initWebSocket();
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html, processor);
+  });
+  // Start server
+  server.begin();
+  touchAttachInterrupt(T3, gotTouch, 40);
+  
+}
+void gotTouch(){
+  touchDetected = true;
+}
+
+void loop() {
+  ws.cleanupClients();
+  digitalWrite(ledPins[0], ledState[0]);
+  digitalWrite(ledPins[1], ledState[1]);
+  digitalWrite(ledPins[2], ledState[2]);
+  if(touchDetected){
+    touchDetected = false;
+    ws.textAll("TOUCH");
+    Serial.println("TOUCH");
+  }
+  if (interruptCounter > 0) {
+    portENTER_CRITICAL(&timerMux);
+    interruptCounter--;
+    portEXIT_CRITICAL(&timerMux);
+
+    totalInterruptCounter++;
+    Serial.print("An interrupt as occurred. Total number: ");
+    Serial.print(totalInterruptCounter); 
+    Serial.print(" Wifi is ");
+    Serial.println(WiFi.status());
+  }  
+}
