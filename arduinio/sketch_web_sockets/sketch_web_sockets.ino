523feb2c5357052dd7ec8b0df7095a06e7b3fe57
tom (tux) <thd@mnet-online.de>
2021-05-09 14:17:31 +0200

    Lots of changes. Moved html out of the way. Completed protocol. Moved LAN-Access out of the way. Added sleeper routine

--- arduinio/sketch_web_sockets/sketch_web_sockets.ino
+++ arduinio/sketch_web_sockets/sketch_web_sockets.ino
@@ -9,150 +9,27 @@
 #include <WiFi.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+#include <DNSServer.h>
+#include <ESPmDNS.h>
 
-// Replace with your network credentials
-const char* ssid = "WLAN 7360";
-const char* password = "41209353118149546763";
 
-//bool ledState = 0; 
+#define DEBUG_ESP_PORT Serial
+#include "config.h"
+
+
+
 bool ledState[3] = {0, 0, 0};
 const int ledPins[3] = {19, 18, 17};
 // Touch is on GPIO15, TOUCH3
 bool touchDetected = false;
+unsigned long msLastEvent;    // millis() of the last event
+
+#ifdef WITH_WATCHDOG
 // Timer section
 volatile int interruptCounter;
 int totalInterruptCounter;
 hw_timer_t *timer = NULL;
 portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
-
-// Create AsyncWebServer object on port 80
-AsyncWebServer server(80);
-AsyncWebSocket ws("/ws");
-
-const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html>
-<head>
-  <title>Blazer Web Server</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" href="data:,">
-  <style>
-  html {
-    font-family: Arial, Helvetica, sans-serif;
-    text-align: center;
-  }
-  h1 {
-    font-size: 1.8rem;
-    color: white;
-  }
-  h2{
-    font-size: 1.5rem;
-    font-weight: bold;
-    color: #143642;
-  }
-  .topnav {
-    overflow: hidden;
-    background-color: #143642;
-  }
-  body {
-    margin: 0;
-  }
-  .content {
-    padding: 30px;
-    max-width: 600px;
-    margin: 0 auto;
-  }
-  .card {
-    background-color: #F8F7F9;;
-    box-shadow: 2px 2px 12px 1px rgba(140,140,140,.5);
-    padding-top:10px;
-    padding-bottom:20px;
-  }
-  .button {
-    padding: 15px 50px;
-    font-size: 24px;
-    text-align: center;
-    outline: none;
-    color: #fff;
-    background-color: #0f8b8d;
-    border: none;
-    border-radius: 5px;
-    -webkit-touch-callout: none;
-    -webkit-user-select: none;
-    -khtml-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-    -webkit-tap-highlight-color: rgba(0,0,0,0);
-   }
-   /*.button:hover {background-color: #0f8b8d}*/
-   .button:active {
-     background-color: #0f8b8d;
-     box-shadow: 2 2px #CDCDCD;
-     transform: translateY(2px);
-   }
-   .state {
-     font-size: 1.5rem;
-     color:#8c8c8c;
-     font-weight: bold;
-   }
-  </style>
-<title>ESP Web Server</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="icon" href="data:,">
-</head>
-<body>
-  <div class="topnav">
-    <h1>ESP WebSocket Server</h1>
-  </div>
-  <div class="content">
-    <div class="card">
-      <h2>Output - GPIO 2</h2>
-      <p class="state">state: <span id="state">%STATE%</span></p>
-      <p><button id="button" class="button">Toggle</button></p>
-    </div>
-  </div>
-<script>
-  var gateway = `ws://${window.location.hostname}/ws`;
-  var websocket;
-  window.addEventListener('load', onLoad);
-  function initWebSocket() {
-    console.log('Trying to open a WebSocket connection...');
-    websocket = new WebSocket(gateway);
-    websocket.onopen    = onOpen;
-    websocket.onclose   = onClose;
-    websocket.onmessage = onMessage; // <-- add this line
-  }
-  function onOpen(event) {
-    console.log('Connection opened');
-  }
-  function onClose(event) {
-    console.log('Connection closed');
-    setTimeout(initWebSocket, 2000);
-  }
-  function onMessage(event) {
-    var state;
-    if (event.data == "1"){
-      state = "ON";
-    }
-    else{
-      state = "OFF";
-    }
-    document.getElementById('state').innerHTML = state;
-  }
-  function onLoad(event) {
-    initWebSocket();
-    initButton();
-  }
-  function initButton() {
-    document.getElementById('button').addEventListener('click', toggle);
-  }
-  function toggle(){
-    websocket.send('toggle');
-  }
-</script>
-</body>
-</html>
-)rawliteral";
 /*
  * Timer Service routine
  */
@@ -162,23 +39,20 @@ void IRAM_ATTR onTimer() {
   portEXIT_CRITICAL_ISR(&timerMux);
  
 }
- 
+#endif
+
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
+DNSServer dnsServer;
+
 
 void notifyClients() {
   char sString[12];
-  sString[0]='S';
-  sString[1]='T';
-  sString[2]='A';
-  sString[3]='T';
-  sString[4]='E';
-
-  sString[5]='0' | ledState[0];
-  sString[6]='0' | ledState[1];
-  sString[7]='0' | ledState[2];
-  sString[8]= 0;
-
-  //sprintf(sString, "STATE%d%d%d", ledState[0], ledState[1], ledState[2]);
+  sprintf(sString, "STATE%d%d%d", ledState[0], ledState[1], ledState[2]);
   ws.textAll(sString);
+  msLastEvent = millis();
 }
 
 void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
@@ -197,21 +71,21 @@ void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
       }
       notifyClients();
     }
-    if((len>3)&&(strncmp((char*)data, "clr", 3)==0)){
+    else if((len>3)&&(strncmp((char*)data, "clr", 3)==0)){
       int s = data[3]-'0';
       if(s>=0 && s<3){
         ledState[s] = 0;
       }
       notifyClients();
     }
-    if((len>6)&&(strncmp((char*)data, "SET",3)==0)){
+    else if((len>6)&&(strncmp((char*)data, "SET",3)==0)){
       for(int i=0; i<3; i++){
         int s = data[3+i];
         ledState[i] = s ? 1 : 0;
       }
       notifyClients();
     }
-    if(strcmp((char*)data, "CLR")==0){
+    else if(strcmp((char*)data, "CLR")==0){
       ledState[0] = ledState[1] = ledState[2]=0;
       notifyClients();
     }
@@ -232,7 +106,9 @@ void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType 
       break;
     case WS_EVT_PONG:
       Serial.printf("PONG\n");
+      break;
     case WS_EVT_ERROR:
+      Serial.printf("ERROR\n");
       break;
   }
 }
@@ -244,6 +120,7 @@ void initWebSocket() {
 
 String processor(const String& var){
   Serial.println(var);
+  /*
   if(var == "STATE"){
     if (ledState){
       return "ON";
@@ -252,45 +129,93 @@ String processor(const String& var){
       return "OFF";
     }
   }
+  */
+  return "Oops";
 }
 
+void onRequest(AsyncWebServerRequest *request){
+  //Handle Unknown Request
+  Serial.println("Request for host "+request->host()+", path "+request->url() );
+  //request->send(404);
+  request->redirect("/");
+}
 void setup(){
   // Serial port for debugging purposes
   Serial.begin(115200);
+  while( !Serial ) { ; }
+  // Setup LEDs
   for(int i=0; i<3; i++){
     pinMode(     ledPins[i], OUTPUT);
     digitalWrite(ledPins[i], LOW);    
   }
-  
-  // Connect to Wi-Fi
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(900);
-    digitalWrite(ledPins[0], HIGH);
-    delay(100);
-    digitalWrite(ledPins[0], LOW);
-    Serial.println("Connecting to WiFi..");
+  esp_sleep_enable_touchpad_wakeup();  
+  touchAttachInterrupt(T3, gotTouch, TOUCH_SENS);
+  if(!MDNS.begin(myName)) {
+     Serial.println("Error starting mDNS");
+  }
+  if( setupWiFiClient(ssid, password, ledPins[1], myName) ){
+    Serial.println("No WiFi found, giving up");
+    goToSleep();    
+  }
+  if(!MDNS.begin(myName)) {
+     Serial.println("Error starting mDNS");
   }
   // Print ESP Local IP Address
   Serial.println(WiFi.localIP());  
+
+#ifdef WITH_WATCHDOG  
   // Timer
   timer = timerBegin(0, 80, true);
   timerAttachInterrupt(timer, &onTimer, true);
-  timerAlarmWrite(timer, 60000000, true);
+  timerAlarmWrite(timer, WATCHDOG_MSG * 1000000, true);
   timerAlarmEnable(timer);
+#endif
+  
   // Web-Server/Sockets
   initWebSocket();
   // Route for root / web page
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send_P(200, "text/html", index_html, processor);
   });
+  server.on("/generate_204", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(204, "", "");
+  });
+  server.onNotFound(onRequest);
   // Start server
   server.begin();
-  touchAttachInterrupt(T3, gotTouch, 40);
-  
+  msLastEvent = millis();
 }
+
 void gotTouch(){
   touchDetected = true;
+}
+void goToSleep(){
+  DEBUG_MSG("Going to sleep now\n");
+  ws.textAll("CLOSE");
+  // blink 4 seconds to give glients time to close
+  // the connection
+  for(int i=0; i<4; i++){
+    digitalWrite(ledPins[2], HIGH);
+    delay(100);
+    digitalWrite(ledPins[2], LOW);
+    delay(900);
+  }
+  server.end();
+  esp_deep_sleep_start();
+}
+
+void print_wakeup_reason(){
+  esp_sleep_wakeup_cause_t wakeup_reason;
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+
+  switch(wakeup_reason){
+    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
+    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
+    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); break;
+  }
 }
 
 void loop() {
@@ -301,8 +226,11 @@ void loop() {
   if(touchDetected){
     touchDetected = false;
     ws.textAll("TOUCH");
-    Serial.printf("TOUCH: %d\n", touchRead(T3));
+    DEBUG_MSG("TOUCH: %d\n", touchRead(T3));
+    //Serial.printf("TOUCH: %d\n", touchRead(T3));
+    msLastEvent = millis();
   }
+#ifdef WITH_WATCHDOG  
   if (interruptCounter > 0) {
     portENTER_CRITICAL(&timerMux);
     interruptCounter--;
@@ -313,4 +241,10 @@ void loop() {
     Serial.print(totalInterruptCounter);
     Serial.printf(" Touch: %d, WiFi is %d\n", touchRead(T3), WiFi.status());
   }  
+#endif
+  if( millis() - msLastEvent > IDLE_TO_SLEEP * 1000L){
+    Serial.printf("Loop: going to sleep, millis=%ld, msLastEvent=%ld\n", millis(), msLastEvent);
+    goToSleep();
+  }
+  
 }
