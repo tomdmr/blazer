4cf19781d5fe5a59573b09202cfef080c2620c3e
tom (tux) <thd@mnet-online.de>
2021-05-13 18:54:14 +0200

    * Added preferences r/w. Reading network is currently ignored * Fixed bug that could send ESP prematurely to sleep (signed/unsigned fuck-up) * Added handlers for internal pages. * Re-ordered code for (hopefully) better readability

--- arduinio/sketch_web_sockets/sketch_web_sockets.ino
+++ arduinio/sketch_web_sockets/sketch_web_sockets.ino
@@ -11,6 +11,7 @@
 #include <ESPAsyncWebServer.h>
 #include <DNSServer.h>
 #include <ESPmDNS.h>
+#include <Preferences.h>
 
 
 #define DEBUG_ESP_PORT Serial
@@ -22,6 +23,17 @@ bool ledState[3] = {0, 0, 0};
 const int ledPins[3] = {19, 18, 17};
 // Touch is on GPIO15, TOUCH3
 bool touchDetected = false;
+
+
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
+DNSServer dnsServer;
+Preferences preferences;
+bool isAP   = true;
+bool tryExt = false;
+
 unsigned long msLastEvent;    // millis() of the last event
 unsigned long msAPActivity;
 #ifdef WITH_WATCHDOG
@@ -41,20 +53,47 @@ void IRAM_ATTR onTimer() {
 }
 #endif
 
+/************************************************************************/
+void gotTouch(){
+  touchDetected = true;
+}
+/************************************************************************/
+void goToSleep(){
+  DEBUG_MSG("Going to sleep now\n");
+  ws.textAll("CLOSE");
+  // blink 4 seconds to give glients time to close
+  // the connection
+  for(int i=0; i<4; i++){
+    digitalWrite(ledPins[2], HIGH);
+    delay(100);
+    digitalWrite(ledPins[2], LOW);
+    delay(900);
+  }
+  server.end();
+  esp_deep_sleep_start();
+}
+/************************************************************************/
+void print_wakeup_reason(){
+  esp_sleep_wakeup_cause_t wakeup_reason;
+  wakeup_reason = esp_sleep_get_wakeup_cause();
 
-// Create AsyncWebServer object on port 80
-AsyncWebServer server(80);
-AsyncWebSocket ws("/ws");
-DNSServer dnsServer;
-
-
+  switch(wakeup_reason){
+    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
+    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
+    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); break;
+  }
+}
+/************************************************************************/
 void notifyClients() {
   char sString[12];
   sprintf(sString, "STATE%d%d%d", ledState[0], ledState[1], ledState[2]);
   ws.textAll(sString);
   msLastEvent = millis();
 }
-
+/************************************************************************/
 void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
   AwsFrameInfo *info = (AwsFrameInfo*)arg;
   if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
@@ -91,7 +130,7 @@ void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
     }
   }
 }
-
+/************************************************************************/
 void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
              void *arg, uint8_t *data, size_t len) {
   switch (type) {
@@ -112,12 +151,12 @@ void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType 
       break;
   }
 }
-
+/************************************************************************/
 void initWebSocket() {
   ws.onEvent(onEvent);
   server.addHandler(&ws);
 }
-
+/************************************************************************/
 String processor(const String& var){
   Serial.println(var);
   /*
@@ -132,11 +171,16 @@ String processor(const String& var){
   */
   return "Oops";
 }
-
+/************************************************************************/
+/************************************************************************/
+/************************************************************************/
 void setup(){
-  // Serial port for debugging purposes
   Serial.begin(115200);
+  preferences.begin(STORAGE_SPACE, false);
+  loadPreferences();
+  preferences.end();
   while( !Serial ) { ; }
+
   // Setup LEDs
   for(int i=0; i<3; i++){
     pinMode(     ledPins[i], OUTPUT);
@@ -170,50 +214,30 @@ void setup(){
   // Route for root / web page
   server.onNotFound(onRequest);
   server.on("/",     HTTP_ANY, onRoot);
+  server.on("/blazer.css", HTTP_ANY, onCSS);
+  server.on("/blazer2.js", HTTP_ANY, onBlazer2);
+  server.on("/portal.html", HTTP_ANY, onPortal);
+  server.on("/demo01.html", HTTP_ANY, onDemo01);
+  server.on("/demo02.html", HTTP_ANY, onDemo02);
   server.on("/scan", HTTP_ANY, onScan);
   server.on("/generate_204", HTTP_ANY, on204);
   // Start server
   server.begin();
+  savePreferences();
   msAPActivity = msLastEvent = millis();
 }
-
-void gotTouch(){
-  touchDetected = true;
-}
-void goToSleep(){
-  DEBUG_MSG("Going to sleep now\n");
-  ws.textAll("CLOSE");
-  // blink 4 seconds to give glients time to close
-  // the connection
-  for(int i=0; i<4; i++){
-    digitalWrite(ledPins[2], HIGH);
-    delay(100);
-    digitalWrite(ledPins[2], LOW);
-    delay(900);
-  }
-  server.end();
-  esp_deep_sleep_start();
-}
-
-void print_wakeup_reason(){
-  esp_sleep_wakeup_cause_t wakeup_reason;
-  wakeup_reason = esp_sleep_get_wakeup_cause();
-
-  switch(wakeup_reason){
-    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
-    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
-    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
-    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
-    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
-    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); break;
-  }
-}
-
+/************************************************************************/
+/************************************************************************/
 void loop() {
+  // Always
   ws.cleanupClients();
   digitalWrite(ledPins[0], ledState[0]);
   digitalWrite(ledPins[1], ledState[1]);
   digitalWrite(ledPins[2], ledState[2]);
+  // if /scan got submitted
+  if( tryExt ){
+    tryExt = false;
+  }
   if(touchDetected){
     touchDetected = false;
     ws.textAll("TOUCH");
@@ -233,9 +257,27 @@ void loop() {
     Serial.printf(" Touch: %d, WiFi is %d\n", touchRead(T3), WiFi.status());
   }  
 #endif
-  if( millis() - msLastEvent > IDLE_TO_SLEEP * 1000L){
+  if( isAP && (millis() - msAPActivity > AP_GRACEPERIOD)){
+    Serial.println("Shutting down AP");
+    isAP = false;
+    WiFi.enableAP(false);
+    msLastEvent = millis();
+  }
+
+  if( !isAP && (millis() > msLastEvent + IDLE_TO_SLEEP)){
     Serial.printf("Loop: going to sleep, millis=%ld, msLastEvent=%ld\n", millis(), msLastEvent);
     goToSleep();
-  }
+  } 
+}
+
+void loadPreferences(){
+  preferences.getString("hname", myName,   sizeof(myName) );
+  //preferences.getString("ssid",  extSSID,   sizeof(extSSID));
+  //preferences.getString("pswd",  extPasswd, sizeof(extSSID));
   
+}
+void savePreferences(){
+  preferences.putString("hname",  myName);
+  preferences.putString("ssid", extSSID);
+  preferences.putString("pswd", extPasswd);
 }
